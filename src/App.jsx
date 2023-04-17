ae6961032c9857574c47e0302f5b1e33eefad8aa
taka2929029 <123882466+taka2929029@users.noreply.github.com>
2023-04-18 00:19:08 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./componemts/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [show, setShow] = useState(true);
+
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
+
+  const onClickShow = () => {
+    setShow(!show);
+  };
+
+  //最初の一回だけ
+  // useEffect(() => {
+  //   console.log(222);
+  // }, []);
+
+  //numが変化したときだけ
+  useEffect(() => {
+    if (num % 3 === 0) {
+      show || setShow(true);
+    } else {
+      show && setShow(false);
+    }
+  }, [num]);
 
   const contentLedyStyle = {
     color: "pink",
@@ -17,7 +38,10 @@ const App = () => {
       <ColorfulMessage testcolor="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage testcolor="pink" message="お元気です!" />
       {/* <p style={contentLedyStyle}>元気です</p> */}
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickButton}>カウント</button>
+      <button onClick={onClickShow}>on off</button>
+      <p>{num}</p>
+      {show && <p>絵文字表示</p>}
     </>
   );
 };
